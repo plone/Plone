dad515d845bf4d393e583bf9189418eb3230322d
Daniel Nouri <daniel.nouri@gmail.com>
2008-10-23 15:10:57 +0000

    Add a migration that sets the permission of the iterate_checkin button action to View (from Modify portal contents).
    svn path=/Plone/branches/3.1/; revision=22931

---
+++ Products/CMFPlone/migrations/v3_1/final_three1x.py
@@ -0,0 +1,18 @@
+from Products.CMFPlone.migrations.migration_util import loadMigrationProfile
+
+def three11_three12(portal):
+    """3.1.1 -> 3.1.2"""
+    
+    out = []
+    
+    loadMigrationProfile(portal, 'profile-Products.CMFPlone.migrations:3.1.1-3.1.2')
+
+def three14_three15(portal):
+    """3.1.4 -> 3.1.5"""
+    
+    out = []
+    
+    loadMigrationProfile(portal, 'profile-Products.CMFPlone.migrations:3.1.3-3.1.4')
+
+def three16_three17(portal):
+    loadMigrationProfile(portal, 'profile-Products.CMFPlone.migrations:3.1.6-3.1.7')
