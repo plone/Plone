e5f65c45c2e613183ddc5138f0405dcf713bd916
Daniel Nouri <daniel.nouri@gmail.com>
2008-10-23 17:00:24 +0000

    Wichert sez this migration should happen in 3.2.
    svn path=/Plone/branches/3.1/; revision=22938

--- Products/CMFPlone/migrations/v3_1/final_three1x.py
+++ Products/CMFPlone/migrations/v3_1/final_three1x.py
@@ -13,10 +13,3 @@ def three14_three15(portal):
     out = []
     
     loadMigrationProfile(portal, 'profile-Products.CMFPlone.migrations:3.1.3-3.1.4')
-
-def three16_three17(portal):
-    iterate_installed = filter(
-        lambda info: info['id'] == 'plone.app.iterate',
-        portal.portal_quickinstaller.listInstalledProducts())
-    if iterate_installed:
-        loadMigrationProfile(portal, 'profile-Products.CMFPlone.migrations:3.1.6-3.1.7-iterate')
