13fe94f92c58788b2ff1e1c6cc2a170622c94ad6
David Glick <davidglick@groundwire.org>
2008-10-31 03:35:25 +0000

    per http://bugs.python.org/issue1817, python 2.6's cgi library will now include querystring params when parsing POST requests.  so we need to get rid of the query string in this POST method test so Zope doesn't end up with tuples for the param values
    svn path=/Plone/trunk/; revision=23043

--- Products/CMFPlone/tests/testControlPanelScripts.py
+++ Products/CMFPlone/tests/testControlPanelScripts.py
@@ -29,6 +29,7 @@ class TestNoGETControlPanel(PloneTestCase.FunctionalTestCase):
         self.assertEqual(response.getStatus(), 403)
 
         data = StringIO(qstring)
+        del env['QUERY_STRING']
         response = self.publish(path, basic_auth, env, request_method='POST',
                                 stdin=data)
         self.assertEqual(response.getStatus(), success)
