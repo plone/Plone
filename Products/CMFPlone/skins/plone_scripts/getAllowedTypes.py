c4efa48a95a642f715b968ffc67480597c1ef637
Hanno Schlichting <hanno@hannosch.eu>
2008-11-01 08:46:37 +0000

    Revert part of r22987, as those scripts are still in use.
    svn path=/Plone/trunk/; revision=23054

--- Products/CMFPlone/skins/plone_scripts/getAllowedTypes.py
+++ Products/CMFPlone/skins/plone_scripts/getAllowedTypes.py
@@ -5,8 +5,9 @@
 ##bind script=script
 ##bind subpath=traverse_subpath
 ##parameters=
-##title=Return a list of the content types allowed here
+##title=Return a list of the content types allowed here filtered by getNotAddableTypes
 
+filterOut = context.getNotAddableTypes()
 types = context.sortObjects( context.allowedContentTypes() )
 
-return [ ctype for ctype in types ]
+return [ ctype for ctype in types if ctype.getId() not in filterOut ]
