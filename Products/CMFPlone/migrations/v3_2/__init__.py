c0d6378a107ec9528ba9fee0002a7806a024c405
Wichert Akkerman <wichert@wiggy.net>
2008-11-23 23:17:32 +0000

    Rename the migrations a bit
    svn path=/Plone/branches/3.2/; revision=23430

--- Products/CMFPlone/migrations/v3_2/__init__.py
+++ Products/CMFPlone/migrations/v3_2/__init__.py
@@ -1,1 +1,12 @@
-import betas
+from Products.CMFCore.utils import getToolByName
+from Products.CMFPlone.migrations.migration_util import loadMigrationProfile
+
+def alpha1_rc1(portal):
+    """3.1.7 -> 3.2beta1
+    """
+    actions = getToolByName(portal, 'portal_actions')
+    if 'iterate_checkin' in actions.object_buttons.objectIds():
+        loadMigrationProfile(
+            portal,
+            'profile-Products.CMFPlone.migrations:3.2a1-3.2rc1-iterate')
+
