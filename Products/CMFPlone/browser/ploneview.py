b296c5931a19d1f7be2c18c62fc641889da616d3
Calvin Hendryx-Parker <calvin@sixfeetup.com>
2009-01-13 16:42:03 +0000

    * Added time_only for use with toLocalizedTime so that event_view now localizes the start/end times if the start/end dates are the same.  * Added migration for 3.2 to 3.2.1 to add new property to the propery tool.  * Added new time_format id to the po files to support the new time_only fix  * thx jnelson, fixes #8607
    svn path=/Plone/branches/3.2/; revision=24257

--- Products/CMFPlone/browser/ploneview.py
+++ Products/CMFPlone/browser/ploneview.py
@@ -182,12 +182,12 @@ class Plone(BrowserView):
 
     # Utility methods
     
-    def toLocalizedTime(self, time, long_format=None):
+    def toLocalizedTime(self, time, long_format=None, time_only=None):
         """Convert time to localized time
         """
         context = aq_inner(self.context)
         util = getToolByName(context, 'translation_service')
-        return util.ulocalized_time(time, long_format, context=context,
+        return util.ulocalized_time(time, long_format, time_only, context=context,
                                     domain='plonelocales', request=self.request)
     
     @memoize
