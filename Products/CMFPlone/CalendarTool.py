a98ae5af67e6c8ab3d52e6d30da5da77f080b6e4
Calvin Hendryx-Parker <calvin@sixfeetup.com>
2009-02-08 06:03:49 +0000

    * Fixed the calendar tool to not rely on self.context  * Added the passing of keyword args to the calendar tool  * Added tests to the calendar tool to verify that you can pass a path as a keyword
    svn path=/Plone/branches/calvinhp-inavigationroot-fixes/; revision=24893

--- Products/CMFPlone/CalendarTool.py
+++ Products/CMFPlone/CalendarTool.py
@@ -38,8 +38,38 @@ class CalendarTool(PloneBaseTool, BaseTool):
         firstweekday = self._getCalendar().firstweekday()+1
         return [i%7 for i in range(firstweekday, firstweekday + 7)]
 
+    security.declarePublic('getEventsForCalendar')
+    def getEventsForCalendar(self, month='1', year='2002', **kw):
+        """ recreates a sequence of weeks, by days each day is a mapping.
+            {'day': #, 'url': None}
+        """
+        year = int(year)
+        month = int(month)
+        # daysByWeek is a list of days inside a list of weeks, like so:
+        # [[0, 1, 2, 3, 4, 5, 6],
+        #  [7, 8, 9, 10, 11, 12, 13],
+        #  [14, 15, 16, 17, 18, 19, 20],
+        #  [21, 22, 23, 24, 25, 26, 27],
+        #  [28, 29, 30, 31, 0, 0, 0]]
+        daysByWeek = self._getCalendar().monthcalendar(year, month)
+        weeks = []
+
+        events = self.catalog_getevents(year, month, **kw)
+
+        for week in daysByWeek:
+            days = []
+            for day in week:
+                if events.has_key(day):
+                    days.append(events[day])
+                else:
+                    days.append({'day': day, 'event': 0, 'eventslist':[]})
+
+            weeks.append(days)
+
+        return weeks
+
     security.declarePublic('catalog_getevents')
-    def catalog_getevents(self, year, month):
+    def catalog_getevents(self, year, month, **kw):
         """ given a year and month return a list of days that have events 
         """
         # XXX: this method violates the rules for tools/utilities:
@@ -49,17 +79,18 @@ class CalendarTool(PloneBaseTool, BaseTool):
         last_day = self._getCalendar().monthrange(year, month)[1]
         first_date = self.getBeginAndEndTimes(1, month, year)[0]
         last_date = self.getBeginAndEndTimes(last_day, month, year)[1]
-        # Take the NavigationRoot into consideration when doing the query
-        path = getNavigationRoot(self.context)
+        
+        query_args = {
+            'portal_type': self.getCalendarTypes(),
+            'review_state': self.getCalendarStates(),
+            'start': {'query': last_date, 'range': 'max'},
+            'end': {'query': first_date, 'range': 'min'},
+            'sort_on': 'start'
+        }
+        query_args.update(kw)
         
         ctool = getToolByName(self, 'portal_catalog')
-        query = ctool(
-                        portal_type=self.getCalendarTypes(),
-                        review_state=self.getCalendarStates(),
-                        start={'query': last_date, 'range': 'max'},
-                        end={'query': first_date, 'range': 'min'},
-                        path=path,
-                        sort_on='start' )
+        query = ctool(**query_args)
 
         # compile a list of the days that have events
         eventDays={}
