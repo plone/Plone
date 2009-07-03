5abf581adae28800780755c36458ee0c98019456
David Glick <davidglick@groundwire.org>
2009-07-03 21:17:03 +0000

    fix one more ex-template global, and we can't use string exceptions in Python 2.6
    svn path=/Plone/branches/davisagli-4.0-trunk-backports/; revision=27802

--- Products/CMFPlone/__init__.py
+++ Products/CMFPlone/__init__.py
@@ -69,6 +69,9 @@ def initialize(context):
     # Make Unauthorized importable TTW
     ModuleSecurityInfo('AccessControl').declarePublic('Unauthorized')
 
+    # Make Forbidden importable TTW
+    ModuleSecurityInfo('zExceptions').declarePublic('Forbidden')
+
     # Make ConflictError importable TTW
     ModuleSecurityInfo('ZODB.POSException').declarePublic('ConflictError')
 
