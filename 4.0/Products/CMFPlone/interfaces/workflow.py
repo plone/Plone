6d584de8579df194a5d58b01cb8aaffb8a7d0836
Manabu Terada <terada@cmscom.jp>
2009-09-30 06:26:06 +0000

    adding miss
    svn path=/Plone/branches/plip9309-unicodesplitter/; revision=30030

--- 4.0/Products/CMFPlone/interfaces/workflow.py
+++
@@ -1,5 +0,0 @@
-from zope.interface.common.sequence import IReadSequence
-
-class IWorkflowChain( IReadSequence ):
-    """ an interface denoting the cmf workflow name sequence
-    """
--- 4.0/Products/CMFPlone/skins/plone_ecmascript/first_input_focus.js
+++
@@ -1,5 +0,0 @@
-// Focus on error or first element in a form with class="enableAutoFocus"
-jq(function() {
-    if (jq("form div.error :input:first").focus().length) return;
-    jq("form.enableAutoFocus :input:not(.formTabs):visible:first").focus();
-});
