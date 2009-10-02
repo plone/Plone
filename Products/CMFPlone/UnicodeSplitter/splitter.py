3b1b60511ec3a515ad04823233b4bc8f62588631
Manabu Terada <terada@cmscom.jp>
2009-10-02 10:14:44 +0000

    UnicodeSplitter translate docstr Japanese to English
    svn path=/Plone/branches/plip9309-unicodesplitter/; revision=30115

--- Products/CMFPlone/UnicodeSplitter/splitter.py
+++ Products/CMFPlone/UnicodeSplitter/splitter.py
@@ -17,21 +17,22 @@ from Products.CMFPlone.utils import getSiteEncoding
 from Products.CMFPlone.UnicodeSplitter.config import rx_U, rxGlob_U, \
             rx_L, rxGlob_L, rx_all, pattern, pattern_g
 
-def bigram(u,limit=1):
-    """ バイグラムに分ける。limit=0 なら
-    日本人-> [日本,本人, 人]
-    金 -> [金]
-    limit = 1なら
-    日本人-> [日本,本人]
-    金 -> []
+def bigram(u, limit=1):
+    """ Split into bi-gram.
+    limit arg describes ending process.
+    If limit = 0 then
+        日本人-> [日本,本人, 人]
+        金 -> [金]
+    If limit = 1 then
+        日本人-> [日本,本人]
+        金 -> []
     """
     return [u[i:i+2] for i in xrange(len(u) - limit)]
 
-def process_str_post(s,enc):
-    """strの受け取り、?と*を取り除き、
-    strを返す。
-    decodeできればユニコードとして処理し、
-    失敗すれば、asciiとして処理する。
+def process_str_post(s, enc):
+    """Receive str, remove ? and *, then return str.
+    If decode gets sucessful, process str as unicode.
+    If decode gets failed, process str as ASCII.
     """
     try:
         uni = s.decode(enc, "strict")
@@ -43,11 +44,11 @@ def process_str_post(s,enc):
         return s.replace("?", "").replace("*", "")
 
 def process_str(s, enc):
-    """ strとエンコードを受け取り、
-    バイグラム化した結果として、strのlistを返す。
-    ユニコードにdecodeし、process_unicodeに渡す。
-    decodeに失敗した場合は、rx_Lにより、
-    ロケールに合わせたword単位でスプリットした結果を返す。
+    """ Receive str and encoding, then return the list 
+    of str as bi-grammed result.
+    Decode str into unicode and pass it to process_unicode.
+    When decode failed, return the result splitted per word.
+    Splitting depends on locale specified by rx_L.
     """
     try:
         uni = s.decode(enc, "strict")
@@ -57,12 +58,11 @@ def process_str(s, enc):
     return [x.encode(enc, "strict") for x in bigrams]
 
 def process_str_glob(s, enc):
-    """ strとエンコードを受け取り、
-    バイグラム化した結果として、strのlistを返す。
-    globを考慮して処理する。
-    ユニコードにdecodeし、process_unicode_globに渡す。
-    decodeに失敗した場合は、rxGlob_Lにより、
-    ロケールに合わせたword単位でスプリットした結果を返す。
+    """ Receive str and encoding, then return the list
+    of str considering glob processing.
+    Decode str into unicode and pass it to process_unicode_glob.
+    When decode failed, return the result splitted per word.
+    Splitting depends on locale specified by rxGlob_L.
     """
     try:
         uni = s.decode(enc, "strict")
@@ -72,8 +72,8 @@ def process_str_glob(s, enc):
     return [x.encode(enc, "strict") for x in bigrams]
 
 def process_unicode(uni):
-    """ ユニコード文字列を受け取り、
-    バイグラム化した結果として、ユニコードのlistを返す。
+    """ Receive unicode string, then return a list of unicode
+    as bi-grammed result.
     """
     normalized = unicodedata.normalize('NFKC', uni)
     for word in rx_U.findall(normalized):
@@ -86,9 +86,8 @@ def process_unicode(uni):
                     yield x
 
 def process_unicode_glob(uni):
-    """ ユニコード文字列を受け取り、
-    バイグラム化した結果として、ユニコードのlistを返す。
-    globを考慮して処理する。
+    """ Receive unicode string, then return a list of unicode
+    as bi-grammed result.  Considering globbing.
     """
     normalized = unicodedata.normalize('NFKC', uni)
     for word in rxGlob_U.findall(normalized):
@@ -115,27 +114,27 @@ class Splitter(object):
     implements(ISplitter)
     
     def process(self, lst):
-        """Index時に呼ばれる
-        strのlistを受け取り、バイグラムの処理を行い
-        strのlistを返す。
+        """ Will be called when indexing.
+        Receive list of str, make it bi-grammed, then return
+        the list of str.
         """
         enc = getSiteEncoding(self)
         result = [x for s in lst for x in process_str(s, enc)]
         return result
     
     def processGlob(self, lst):
-        """Search時の1回目に呼ばれる
-        strのlistを受け取り、バイグラムのglobを考慮した処理を行い
-        strのlistを返す。
+        """ Will be called once when searching.
+        Receive list of str, make it bi-grammed considering
+        globbing, then return the list of str.
         """
         enc = getSiteEncoding(self)
         result = [x for s in lst for x in process_str_glob(s, enc)]
         return result
 
     def process_post_glob(self, lst):
-        """Search時の2回目に呼ばれる
-        strのlistを受け取り、?と*を取り除き、
-        strのlistを返す。
+        """ Will be called twice when searching.
+        Receive list of str, Remove ? and *, then return
+        the list of str.
         """
         enc = getSiteEncoding(self)
         result = [process_str_post(s, enc) for s in lst]
