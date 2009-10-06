91c0bba0bfcad5465c957476d24779e0ee2bd87e
Hanno Schlichting <hanno@hannosch.eu>
2009-10-06 22:02:41 +0000

    Extend the language specific default configuration to set a reasonable first day of week on the calendar tool. Americans can get their Sunday back while Europeans still enjoy their proper Monday. Peace :)
    svn path=/Plone/branches/4.0/; revision=30233

--- Products/CMFPlone/setuphandlers.py
+++ Products/CMFPlone/setuphandlers.py
@@ -182,6 +182,22 @@ class PloneGenerator:
                 setUseCombinedLanguageCodes=use_combined,
                 startNeutral=False)
 
+            # Set the first day of the week, defaulting to Sunday, as the
+            # locale data doesn't provide a value for English. European
+            # languages / countries have an entry of Monday, though.
+            calendar = getToolByName(p, "portal_calendar", None)
+            if calendar is not None:
+                first = 6
+                gregorian = locale.dates.calendars.get(u'gregorian', None)
+                if gregorian is not None:
+                    first = gregorian.week.get('firstDay', None)
+                    # on the locale object we have: mon : 1 ... sun : 7
+                    # on the calendar tool we have: mon : 0 ... sun : 6
+                    if first is not None:
+                        first = first - 1
+
+                calendar.firstweekday = first
+
             # Enable visible_ids for non-latin scripts
 
             # See if we have an url normalizer
