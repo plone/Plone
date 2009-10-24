4f40a6ef19fc26a83bf35d2db1bd206cec93c438
Hanno Schlichting <hanno@hannosch.eu>
2009-10-24 20:08:40 +0000

    Added option to skip the creation of the default example content in the new add site form. Grouped the add-ons selection via a fieldset.
    svn path=/Plone/branches/4.0/; revision=30856

--- Products/CMFPlone/browser/admin.py
+++ Products/CMFPlone/browser/admin.py
@@ -108,6 +108,7 @@ class AddPloneSite(BrowserView):
                 title=form.get('title', ''),
                 profile_id=form.get('profile_id', _DEFAULT_PROFILE),
                 extension_ids=form.get('extension_ids', ()),
+                setup_content=form.get('setup_content', False),
                 )
             self.request.response.redirect(site.absolute_url())
 
