10eb042f1d012b0a251222cd8ea2e79fa1c25e13
David Glick <davidglick@groundwire.org>
2009-11-09 04:00:09 +0000

    make sure the mock mailhost can be found via getToolByName in tests
    svn path=/Plone/trunk/; revision=31222

--- Products/CMFPlone/tests/test_mails.py
+++ Products/CMFPlone/tests/test_mails.py
@@ -8,6 +8,9 @@ from zope.testing import doctest
 from Testing.ZopeTestCase import FunctionalDocFileSuite
 from Products.CMFPlone.tests import PloneTestCase
 
+from Acquisition import aq_base
+from zope.component import getSiteManager
+from Products.MailHost.interfaces import IMailHost
 from Products.CMFPlone.tests.utils import MockMailHost
 
 OPTIONFLAGS = (doctest.ELLIPSIS |
@@ -17,10 +20,16 @@ class MockMailHostTestCase(PloneTestCase.FunctionalTestCase):
 
     def afterSetUp(self):
         self.portal._original_MailHost = self.portal.MailHost
-        self.portal.MailHost = MockMailHost('MailHost')
+        self.portal.MailHost = mailhost = MockMailHost('MailHost')
+        sm = getSiteManager(context=self.portal)
+        sm.unregisterUtility(provided=IMailHost)
+        sm.registerUtility(mailhost, provided=IMailHost)
 
     def beforeTearDown(self):
         self.portal.MailHost = self.portal._original_MailHost
+        sm = getSiteManager(context=self.portal)
+        sm.unregisterUtility(provided=IMailHost)
+        sm.registerUtility(aq_base(self.portal._original_MailHost), provided=IMailHost)
 
 
 def test_suite():
