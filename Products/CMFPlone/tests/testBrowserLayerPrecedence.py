faba68060309563d80a7ba6dd9ef114b6db3ec2f
David Glick <davidglick@groundwire.org>
2009-11-27 21:47:59 +0000

    fix the layer precedence tests to demonstrate that we still have an issue when the theme layer extends IPloneDefaultLayer but the browser layer doesn't. refs #9421
    svn path=/Plone/branches/3.3/; revision=32103

--- Products/CMFPlone/tests/testBrowserLayerPrecedence.py
+++ Products/CMFPlone/tests/testBrowserLayerPrecedence.py
@@ -10,42 +10,70 @@ from zope.interface import Interface
 from zope.component import getGlobalSiteManager
 from zope.publisher.interfaces.browser import IBrowserSkinType, IDefaultBrowserLayer
 from zope.app.publication.interfaces import BeforeTraverseEvent
+from Products.CMFDefault.interfaces import ICMFDefaultSkin
 from plone.browserlayer.utils import register_layer, unregister_layer
+from plone.theme.interfaces import IDefaultPloneLayer
 
-class IThemeSpecific(Interface):
+class IThemeSpecific(IDefaultPloneLayer):
     pass
 
 class IAdditiveLayer(Interface):
     pass
 
-class TestBrowserLayerPrecedence(PloneTestCase.FunctionalTestCase):
+class IAdditiveLayerExtendingDefault(IDefaultPloneLayer):
+    pass
 
+class LayerPrecedenceTestCase(PloneTestCase.FunctionalTestCase):
+
+    additive_layer = None
+    theme_layer = None
+
+    def afterSetUp(self):
+        register_layer(self.additive_layer, 'Plone.testlayer')
+        gsm = getGlobalSiteManager()
+        self._old_theme_layer = gsm.queryUtility(IBrowserSkinType, name='Plone Default')
+        gsm.registerUtility(self.theme_layer, IBrowserSkinType, 'Plone Default')
+    
     def _get_request_interfaces(self):
         request = TestRequest()
         notify(BeforeTraverseEvent(self.portal, request))
         iro = list(request.__provides__.__iro__)
         return iro
-
-    def testCustomBrowserLayerHasPrecedenceOverDefaultLayer(self):
-        register_layer(IAdditiveLayer, 'Plone.testlayer')
+    
+    def testLayerPrecedence(self):
         iro = self._get_request_interfaces()
-        unregister_layer('Plone.testlayer')
+        theme_layer_pos = iro.index(self.theme_layer)
+        additive_layer_pos = iro.index(self.additive_layer)
+        plone_default_pos = iro.index(IDefaultPloneLayer)
+        cmf_default_pos = iro.index(ICMFDefaultSkin)
+        zope_default_pos = iro.index(IDefaultBrowserLayer)
         
-        self.failUnless(iro.index(IAdditiveLayer) < iro.index(IDefaultBrowserLayer))
-
-    def testThemeSpecificLayerTakesHighestPrecedence(self):
+        # We want to have the theme layer first, followed by additive layers,
+        # followed by default layers.
+        self.assertEqual(theme_layer_pos, 0)
+        self.failUnless(theme_layer_pos < additive_layer_pos)
+        self.failUnless(additive_layer_pos < plone_default_pos)
+        self.failUnless(plone_default_pos < cmf_default_pos)
+        self.failUnless(cmf_default_pos < zope_default_pos)
+    
+    def beforeTearDown(self):
+        unregister_layer('Plone.testlayer')
         gsm = getGlobalSiteManager()
-        gsm.registerUtility(IThemeSpecific, IBrowserSkinType, 'Plone Default')
-        register_layer(IAdditiveLayer, 'Plone.testlayer')
-        iro = self._get_request_interfaces()
-        gsm.unregisterUtility(IThemeSpecific, IBrowserSkinType, 'Plone Default')
-        unregister_layer('Plone.testlayer')
-        
-        self.failUnless(iro.index(IThemeSpecific) < iro.index(IAdditiveLayer),
-            'Theme-specific browser layers should take precedence over other browser layers.')
+        self.assertEqual(True, gsm.unregisterUtility(provided=IBrowserSkinType, name='Plone Default'))
+        if self._old_theme_layer is not None:
+            gsm.registerUtility(self._old_theme_layer, IBrowserSkinType, 'Plone Default')
+
+class TestPrecedenceWithAdditiveLayerExtendingInterface(LayerPrecedenceTestCase):
+    theme_layer = IThemeSpecific
+    additive_layer = IAdditiveLayer
+
+class TestPrecedenceWithAdditiveLayerExtendingDefault(LayerPrecedenceTestCase):
+    theme_layer = IThemeSpecific
+    additive_layer = IAdditiveLayerExtendingDefault
 
 def test_suite():
     from unittest import TestSuite, makeSuite
     suite = TestSuite()
-    suite.addTest(makeSuite(TestBrowserLayerPrecedence))
+    suite.addTest(makeSuite(TestPrecedenceWithAdditiveLayerExtendingInterface))
+    suite.addTest(makeSuite(TestPrecedenceWithAdditiveLayerExtendingDefault))    
     return suite
