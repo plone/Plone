plone/Plone

commit fba6df534819fe618ab6e0fd457ef3b70e6ff4de

Make the external_editor link on OS X use the .zem extension. Fixes #7291
svn path=/Plone/branches/3.3/; revision=30188

Products/CMFPlone/skins/plone_scripts/external_edit.py
@@ -1,0 +1,21 @@
+## Script (Python) "external_edit"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=
+##title=http://cmf.zope.org/Members/tseaver/20020723_external_editor_available
+
+from Products.PythonScripts.standard import url_quote
+
+request = context.REQUEST
+
+if 'Mac OS X' in request.get('HTTP_USER_AGENT', ''):
+    return context.REQUEST['RESPONSE'].redirect(
+        '%s/externalEdit_/%s.zem?macosx=1' % (context.aq_parent.absolute_url(),
+                                              url_quote(context.getId())))
+else:
+    return context.REQUEST['RESPONSE'].redirect(
+        '%s/externalEdit_/%s.zem' % (context.aq_parent.absolute_url(),
+                                     url_quote(context.getId())))

commit 02b1fd3e131a98cca92aad2c62257a2729f685f9

fix a regression in the external editor that was introduced in r30188: .zem extension should only be added on OS X. fixes #9888
svn path=/Plone/trunk/; revision=32713

Products/CMFPlone/skins/plone_scripts/external_edit.py
@@ -17,5 +17,5 @@ if 'Mac OS X' in request.get('HTTP_USER_AGENT', ''):
                                               url_quote(context.getId())))
 else:
     return context.REQUEST['RESPONSE'].redirect(
-        '%s/externalEdit_/%s.zem' % (context.aq_parent.absolute_url(),
+        '%s/externalEdit_/%s' % (context.aq_parent.absolute_url(),
                                      url_quote(context.getId())))

Products/CMFPlone/tests/testExternalEditorEnabled.py
@@ -1,9 +1,4 @@
-#
-# Test check_id script
-#
-
 from Products.CMFPlone.tests import PloneTestCase
-
 
 class TestExternalEditorEnabled(PloneTestCase.PloneTestCase):
     '''Tests the externalEditorEnabled script'''
@@ -58,6 +53,17 @@ class TestExternalEditorEnabled(PloneTestCase.PloneTestCase):
         self.failIf(self.doc.externalEditorEnabled())
         self.doc.wl_clearLocks()
         self.failUnless(self.doc.externalEditorEnabled())
+
+    def testExternalEditorUsesZemExtensionForOSX(self):
+        self.doc.external_edit()
+        redirect = self.doc.REQUEST.RESPONSE.headers['location']
+        self.failUnless(redirect.endswith('doc'))
+        
+        # if Mac OS X in the user agent, add a .zem extension
+        self.doc.REQUEST.environ['HTTP_USER_AGENT'] = 'Mac OS X'
+        self.doc.external_edit()
+        redirect = self.doc.REQUEST.RESPONSE.headers['location']
+        self.failUnless(redirect.endswith('.zem?macosx=1'))
 
 
 def test_suite():