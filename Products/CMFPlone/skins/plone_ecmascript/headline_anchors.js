728fbc3ba95ec8280387ebc1940624930e186f74
Aaron VanDerlip <avanderlip@gmail.com>
2010-01-16 01:23:34 +0000

    added headline_anchors.js to enabled automatic anchors on all headlines, addresses #8701
    svn path=/Plone/trunk/; revision=33128

---
+++ Products/CMFPlone/skins/plone_ecmascript/headline_anchors.js
@@ -0,0 +1,19 @@
+/* Scan all headlines in the #content area 
+* add a generated anchor and link based on title 
+*/
+
+(function($) { $(function() {
+  var content = getContentArea();
+  if (!content) return;
+    //find all headers in content area, filter matched set to remove any pre-existing anchors
+    var headers =  $(content).find(':header').not(':header:has(a)'); 
+    headers.each(function(){ 
+        //clean text to strip illegal URL characters
+        var friendly_title = $(this).text().replace(/[\<\>!@#\$%^&\*,\s]/g,'-');
+        $(this).wrapInner('<a class="link-anchor" href=#' + friendly_title + ' name=' + friendly_title + '></a>');
+      }     
+      );
+
+}); })(jQuery);
+
+
