8fe1c78f3dbe2300cc5bfa218d4c0f5ec77a5385
Aaron VanDerlip <avanderlip@gmail.com>
2010-01-16 04:15:18 +0000

    fixes for string cleaning so now cross browser compatible, calling content area directly instead of relying on deprecated functions.
    svn path=/Plone/trunk/; revision=33135

--- Products/CMFPlone/skins/plone_ecmascript/headline_anchors.js
+++ Products/CMFPlone/skins/plone_ecmascript/headline_anchors.js
@@ -3,17 +3,20 @@
 */
 
 (function($) { $(function() {
-  var content = getContentArea();
-  if (!content) return;
-    //find all headers in content area, filter matched set to remove any pre-existing anchors
-    var headers =  $(content).find(':header').not(':header:has(a)'); 
-    headers.each(function(){ 
-        //clean text to strip illegal URL characters
-        var friendly_title = $(this).text().trim().replace(/[\<\>!@#\$%^&\*,\s]/g,'-');
-        $(this).wrapInner('<a class="link-anchor" href=#' + friendly_title + ' name=' + friendly_title + '></a>');
-      }     
-      );
 
-}); })(jQuery);
+  var content = $('#region-content,#content');
+  if (!content) return;
+  //find all headers in content area, filter matched set to remove any pre-existing anchors
+  var headers =  $(content).find(':header').not(':header:has(a)'); 
+  headers.each(function(){ 
+    //remove white space and carriage returns 
+    var friendly_title = $(this).text().replace(/^\s+|\s+$/g,"");
+    //clean text to strip illegal URL characters
+    friendly_title = friendly_title.replace(/[\<\>!@#\$%^&\*,\s]/g,'-');
+    $(this).wrapInner('<a class="link-anchor" href="#' + friendly_title + '" name="' + friendly_title + '"></a>');
+  }     
+  );
+});
+})(jQuery);
 
 
