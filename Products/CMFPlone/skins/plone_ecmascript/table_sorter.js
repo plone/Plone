ca595d6a0bb86fa624ffa15a5623d7eed3565ac2
Alexander Limi <limi@plone.org>
2010-03-18 05:40:56 +0000

    Updated the table_sorter.js to use Unicode for its arrows instead of images. Also made it more robust, so it doesn't fail if you have two columns with the same name. This refs http://dev.plone.org/plone/ticket/10352
    svn path=/Plone/trunk/; revision=35214

--- Products/CMFPlone/skins/plone_ecmascript/table_sorter.js
+++ Products/CMFPlone/skins/plone_ecmascript/table_sorter.js
@@ -1,6 +1,8 @@
 
 /********* Table sorter script *************/
 
+(function($) {
+
 function sortable(a) {
     // convert a to something sortable
     // A number, but not a date?
@@ -10,21 +12,22 @@ function sortable(a) {
 }
 
 function sort() {
-    var name = jQuery(this).text();
-    var table = jQuery(this).parents('table:first');
+	var th = $(this).closest('th');
+	var colnum = $('th', $(this).closest('thead')).index(th);
+    var table = $(this).parents('table:first');
     var tbody = table.find('tbody:first');
-    var reverse = table.attr('sorted') == name;
+    var reverse = table.attr('sorted') == colnum;
 
-    jQuery(this).parent().find('th:not(.nosort) img.sortdirection')
-        .attr('src', portal_url + '/arrowBlank.png');
-    jQuery(this).children('img.sortdirection').attr('src', portal_url + 
-        (reverse ? '/arrowDown.png' : '/arrowUp.png'));
+    $(this).parent().find('th:not(.nosort) .sortdirection')
+        .html('&#x2003;');
+    $(this).children('.sortdirection').html(
+        reverse ? '&#x25b2;' : '&#x25bc;');
     
-    var index = jQuery(this).parent().children('th').index(this);
+    var index = $(this).parent().children('th').index(this);
     var data = [];
     var usenumbers = true;
     tbody.find('tr').each(function() {
-        var cells = jQuery(this).children('td');
+        var cells = $(this).children('td');
         var sortableitem = sortable(cells.slice(index,index+1).text());
         if (isNaN(sortableitem)) usenumbers = false;
         data.push([
@@ -40,10 +43,10 @@ function sort() {
         else
             data.sort();
         if (reverse) data.reverse();
-        table.attr('sorted', reverse ? '' : name);
+        table.attr('sorted', reverse ? '' : colnum);
 
         // appending the tr nodes in sorted order will remove them from their old ordering
-        tbody.append(jQuery.map(data, function(a) { return a[3]; }));
+        tbody.append($.map(data, function(a) { return a[3]; }));
         // jquery :odd and :even are 0 based
         tbody.find('tr').removeClass('odd').removeClass('even')
             .filter(':odd').addClass('even').end()
@@ -51,18 +54,18 @@ function sort() {
     }    
 }
 
-jQuery(function() {
+$(function() {
     // set up blank spaceholder gif
-    var blankarrow = jQuery('<img>')
-        .attr('src', portal_url + '/arrowBlank.png')
-        .attr('width', 6).attr('height', 9).addClass('sortdirection');
+    var blankarrow = $('<span>&#x2003;</span>').addClass('sortdirection');
     // all listing tables not explicitly nosort, all sortable th cells
     // give them a pointer cursor and  blank cell and click event handler
     // the first one of the cells gets a up arrow instead.
-    jQuery('table.listing:not(.nosort) thead th:not(.nosort)')
+    $('table.listing:not(.nosort) thead th:not(.nosort)')
         .append(blankarrow.clone())
         .css('cursor', 'pointer')
-        .click(sort)
-        .slice(0, 1)
-        .find('img.sortdirection').attr('src', portal_url + '/arrowUp.png');
+        .click(sort);
+
+    
 });
+
+})(jQuery);
