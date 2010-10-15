30a4e4b2883d2d8f80b3070133f1da2fdad87c7e
Patrick Gerken <patrick.gerken@computer.org>
2010-10-15 15:52:42 +0000

    More careful error handling. This fixes #11054
    svn path=/Plone/trunk/; revision=40657

--- Products/CMFPlone/skins/plone_scripts/mail_password.py
+++ Products/CMFPlone/skins/plone_scripts/mail_password.py
@@ -8,10 +8,20 @@
 ##parameters=
 
 from Products.CMFPlone import PloneMessageFactory as pmf
+from AccessControl import Unauthorized
 REQUEST=context.REQUEST
 try:
     response = context.portal_registration.mailPassword(REQUEST['userid'], REQUEST)
 except ValueError, e:
+    try:
+        msg = pmf(e.message)
+    except Unauthorized:
+        try:
+            msg = pmf(str(e))
+        except Unauthorized:
+            # If we are not allowed to tell the user, what is wrong, he
+            # should get an error message and contact the admins
+            raise e
     context.plone_utils.addPortalMessage(pmf(e.message))
     response = context.mail_password_form()
 return response
