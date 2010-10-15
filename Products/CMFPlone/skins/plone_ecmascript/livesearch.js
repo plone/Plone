9c271f0495110ab8f6eea5e5286464a094f65139
Chrissy Wainwright <chrissy@sixfeetup.com>
2010-10-15 15:23:32 +0000

    removed code that applies inline style to the searchbox form.  Styles will be added in the CSS refs #11186
    svn path=/Plone/trunk/; revision=40653

--- Products/CMFPlone/skins/plone_ecmascript/livesearch.js
+++ Products/CMFPlone/skins/plone_ecmascript/livesearch.js
@@ -172,7 +172,6 @@ var livesearch = function (){
         var $key_handler = _keyhandlerfactory($form);
         _search_handlers[$id] = _searchfactory($form, this);
 
-        $form.attr('id', $id).css('white-space', 'nowrap').submit($key_handler.submit);
         jQuery(this).attr('autocomplete','off')
                .keydown($key_handler.handler)
                .focus(_search_handlers[$id].search_delayed)
