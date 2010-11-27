3b5ece72c21ef76fb7e9d7acce9196890bd5ccb3
Laurence Rowe <l@lrowe.co.uk>
2010-11-27 19:12:43 +0000

    new Plone egg
    svn path=/Plone/trunk/; revision=45991

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os.path
+
+version = '4.1a1'
+
+setup(name='Plone',
+      version=version,
+      description="The Plone Content Management System",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "CHANGES.txt")).read(),
+      classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        ],
+      keywords='Plone CMF python Zope',
+      author='Plone Foundation',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://plone.org/',
+      license='GPL version 2',
+      packages=[],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'Products.CMFPlone',
+      ],
+      )
