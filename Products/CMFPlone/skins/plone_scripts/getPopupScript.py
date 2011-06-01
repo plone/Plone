5594005f25897630355b48c07b9a0cdf7657a9a3
Laurence Rowe <l@lrowe.co.uk>
2011-06-01 16:51:59 +0000

    Merge fix for CVE-2011-1948 into 4.0
    svn path=/Plone/branches/4.0/; revision=50165

---
+++ Products/CMFPlone/skins/plone_scripts/getPopupScript.py
@@ -0,0 +1,14 @@
+##parameters=input_id
+from zExceptions import Forbidden
+if container.REQUEST['PUBLISHED'] is script:
+   raise Forbidden('Script may not be published.')
+
+
+return r"""
+
+function returndate(day)
+{
+  opener.document.getElementById('%s').value = day;
+  window.close();
+}
+""" % input_id
