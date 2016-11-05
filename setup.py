e2613ff213fb5f33a3ac855078aea7336d178791
Maurits van Rees <maurits@vanrees.org>
2016-11-05 15:09:58 +0100

    Reindented setup.py.

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ import os.path
 
 version = '4.3.12.dev0'
 
-setup(name='Plone',
-      version=version,
-      description="The Plone Content Management System",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "CHANGES.txt")).read(),
-      classifiers=[
+setup(
+    name='Plone',
+    version=version,
+    description="The Plone Content Management System",
+    long_description=open("README.txt").read() + "\n" +
+                     open(os.path.join("docs", "CHANGES.txt")).read(),
+    classifiers=[
         "Environment :: Web Environment",
         "Framework :: Plone",
         "Framework :: Zope2",
@@ -16,24 +17,24 @@ setup(name='Plone',
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
-        ],
-      keywords='Plone CMF python Zope',
-      author='Plone Foundation',
-      author_email='plone-developers@lists.sourceforge.net',
-      url='http://plone.org/',
-      license='GPL version 2',
-      packages=[],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'Products.CMFPlacefulWorkflow',
-          'Products.CMFPlone',
-          'plone.app.caching',
-          'plone.app.dexterity',
-          'plone.app.iterate',
-          'plone.app.openid',
-          'plone.app.theming',
-          'wicked',
-      ],
-      )
+    ],
+    keywords='Plone CMF python Zope',
+    author='Plone Foundation',
+    author_email='plone-developers@lists.sourceforge.net',
+    url='http://plone.org/',
+    license='GPL version 2',
+    packages=[],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'Products.CMFPlacefulWorkflow',
+        'Products.CMFPlone',
+        'plone.app.caching',
+        'plone.app.dexterity',
+        'plone.app.iterate',
+        'plone.app.openid',
+        'plone.app.theming',
+        'wicked',
+    ],
+)
